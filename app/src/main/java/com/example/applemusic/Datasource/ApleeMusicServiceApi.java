00d5ff79c629b8b0f33d277e95d6b9fdc270e7be
THE FABI7 <fabianguancha13@gmail.com>
2024-05-11 20:37:43 -0500

    front de el buscador

---
+++ app/src/main/java/com/example/applemusic/Datasource/ApleeMusicServiceApi.java
@@ -0,0 +1,27 @@
+package com.example.applemusic.Datasource;
+
+import cafsoft.foundation.URLComponents;
+import cafsoft.foundation.URLQueryItem;
+
+
+public class ApleeMusicServiceApi {
+
+    private URLComponents components = null;
+
+    public ApleeMusicServiceApi() {
+        components = new URLComponents();
+        components.setScheme("https");
+        components.setHost("itunes.apple.com");
+        components.setPath("/search");
+    }
+
+    public void searchSongsByTerm(String searchTerm) {
+        components.setQueryItems(new URLQueryItem[]{
+                new URLQueryItem("media", "music"),
+                new URLQueryItem("entity", "song"),
+                new URLQueryItem("term", searchTerm)
+        });
+    }
+
+
+}
