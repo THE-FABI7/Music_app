a951c3ffc4df3ab1e622133ef875c7c392cd52f8
THE FABI7 <fabianguancha13@gmail.com>
2024-05-16 20:14:28 -0500

    Implementación de la clase result y mejoras en el front de la aplicacion:

--- app/src/main/java/com/example/applemusic/ui/MainActivity.java
+++ app/src/main/java/com/example/applemusic/ui/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.applemusic.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ScrollView;
@@ -17,11 +19,14 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText buscar = null;
 
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
+        initEvents();
 
     }
 
@@ -32,18 +37,33 @@ public class MainActivity extends AppCompatActivity {
 
     public void initEvents(){
         btnSearch.setOnClickListener(view ->{
-         requestMusic();
+            showProgressDialog();
+            requestMusic();
 
         });
     }
 
-    public void requestMusic(){
-        String buscarName =  buscar.getText().toString();
+    public void requestMusic() {
+        String buscarName = buscar.getText().toString();
 
         if (buscarName.isEmpty()) {
-            // Mostrar mensaje de error si el campo está vacío
-            Toast.makeText(MainActivity.this, "Por favor ingrese el nombre de la cancion", Toast.LENGTH_SHORT).show();
+            progressDialog.dismiss();  // Cierra el diálogo si el campo está vacío
+            Toast.makeText(MainActivity.this, "Por favor ingrese el nombre del artista", Toast.LENGTH_SHORT).show();
             return;
         }
+
+        // Simular una operación de red con un retardo
+        new Handler().postDelayed(() -> {
+            progressDialog.dismiss();  // Cierra el diálogo después de completar la búsqueda
+            // Aquí puedes agregar el código para manejar la búsqueda efectiva de la música
+            Toast.makeText(MainActivity.this, "Búsqueda completada", Toast.LENGTH_SHORT).show();
+        }, 2000); // 2000 milisegundos de simulación de búsqueda
+    }
+
+    private void showProgressDialog() {
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Buscando...");
+        progressDialog.setCancelable(false);  // Hacer que el dialogo no sea cancelable
+        progressDialog.show();
     }
 }
